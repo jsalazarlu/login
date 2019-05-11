9e8d8860172bd954d5ff8e8032364995b9a44049
AUTONOMA <jsalazarlu93@gmail.com>
2019-05-11 11:00:35 -0500

    login

---
+++ app/src/main/java/com/example/autonoma/myapplication/ProfesoresActivity.java
@@ -0,0 +1,13 @@
+package com.example.autonoma.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ProfesoresActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profesores);
+    }
+}
